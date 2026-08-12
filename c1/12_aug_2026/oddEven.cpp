83d1fecf2777b00c45226ca9c16351570b051582
mahendratiwari969668 <mahendratiwari969668@gmail.com>
2026-08-12 15:31:53 +0530

    second commit

---
+++ c1/12_aug_2026/oddEven.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int a ;
+    cout<<"Enter the value of a : ";
+    cin>>a;
+    
+    if(a%2==0) cout<<"Number is Even";
+    else cout<<"Number is Odd";
+}
