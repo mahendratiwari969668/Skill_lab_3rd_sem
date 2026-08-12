e08dbb7b92b1580c24a762b27830ac33785145fa
mahendratiwari969668 <mahendratiwari969668@gmail.com>
2026-08-12 15:22:41 +0530

    first commit

---
+++ c1/10_aug_2026/userInput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    cout<<"Enter value of a : ";
+    int a;
+    cin>>a;
+    cout<<"Enter value of b : ";
+    int b;
+    cin>>b;
+    int c = a+b;
+    cout<<c;
+}
---
+++ c1/12_aug_2026/float.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    float a,b ;
+    cout<<"Enter the value of a : ";
+    cin>>a;
+    cout<<"Enter the value of b : ";
+    cin>>b;
+    cout<<"Sum of "<<a<<" + "<<b<<" is "<<a+b;
+}
